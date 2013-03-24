1385715c736f449c4adc3426e4103ba38718f6cf
4ipalino <andyhahn91@gmail.com>
2013-03-24 18:00:54 +0100

    - DuckDuckGo Test

--- src/tests/TestAndy.java
+++ src/tests/TestAndy.java
@@ -1,22 +1,76 @@
 package tests;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 import plagiatssoftware.RabinKarpComparer;
 import plagiatssoftware.RabinKarpComparer.OnSearchFinishedListener;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 
 public class TestAndy
 {
-	private static RabinKarpComparer _RabinCarpComparer;
-	
+	private static RabinKarpComparer	_RabinCarpComparer;
+
 	public static void main(String[] args)
 	{
-		WordProcessing wp = new WordProcessing();
-		
-		//_RabinCarpComparer = new RabinKarpComparer();
-		//test();
+		testDDG();
+
+		// WordProcessing wp = new WordProcessing();
+
+		// _RabinCarpComparer = new RabinKarpComparer();
+		// test();
+	}
+
+	public static void testDDG()
+	{
+		String google = "http://api.duckduckgo.com/?format=xml&q=";
+		String search = "heise";
+		String charset = "UTF-8";
+
+		URL url;
+		try
+		{
+			url = new URL(google + URLEncoder.encode(search, charset));
+			InputStreamReader reader = new InputStreamReader(url.openStream(), charset);
+
+			BufferedReader bufferedReader = new BufferedReader(reader);
+
+			StringBuilder stringBuilder = new StringBuilder();
+			String line = bufferedReader.readLine();
+			while (line != null)
+			{
+				stringBuilder.append(line);
+				line = bufferedReader.readLine();
+			}
+			
+			System.out.println(stringBuilder.toString());
+
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	private static void test()
@@ -48,5 +102,4 @@ public class TestAndy
 			}
 		});
 	}
-
 }
