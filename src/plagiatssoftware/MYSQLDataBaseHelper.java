f048965dee7ae8b9b0ddceddbfa1eb1efd3bc44e
Michi Vo <Michi@Michi-Laptop>
2013-03-25 11:29:53 +0100

    Anpassung an Coding Guidelines

--- src/plagiatssoftware/MYSQLDataBaseHelper.java
+++ src/plagiatssoftware/MYSQLDataBaseHelper.java
@@ -6,31 +6,52 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/MYSQLDataBaseHelper")
 public class MYSQLDataBaseHelper
 {
-	private Connection connect = null;
-	private Statement statement = null;
-	private PreparedStatement preparedStatement = null;
-	private ResultSet resultSet = null;
-	
+	private Connection _connection = null;
+	private Statement _statement = null;
+	private PreparedStatement _preparedStatement = null;
+	private ResultSet _resultSet = null;
+	/**
+	 * Stellt Verbindung zum MySQL Server und der Datenbank her, 
+	 * im Moment wird eine Abfrage der User Vornamen ausgeführt und ausgegeben
+	 * 
+	 * @throws Exception Falls SQL Connect fehlschlägt
+	 */
 	public void connect() throws Exception
 	{
-		// This will load the MySQL driver, each DB has its own driver
+		//Lädt den MySQL Treiber
 	      Class.forName("com.mysql.jdbc.Driver");
-	      // Setup the connection with the DB
-	      connect = DriverManager
+	      //Verbindung mit DB herstellen
+	      _connection = DriverManager
 	          .getConnection("jdbc:mysql://192.168.4.28/plagiatsjaeger?"
 	              + "user=root&password=inf2011#projektAVH");
-	   // Statements allow to issue SQL queries to the database
-	      statement = connect.createStatement();
-	      // Result set get the result of the SQL query
-	      resultSet = statement
+	   //Statements erlauben SQL Abfragen
+	      _statement = _connection.createStatement();
+	      //Abfragenergebnis in ResultSet speichern
+	      _resultSet = _statement
 	          .executeQuery("select * from plagiatsjaeger.user");
-	      while (resultSet.next()) {
-	    	  String user = resultSet.getString(2);
-	    	  System.out.println("Name: " + user);
-	      }
-	      statement.close();
+	      //Für jeden User auf Konsole ausgeben
+	      while (_resultSet.next()) {
+	    	  String strUser = _resultSet.getString(2);
+	    	  System.out.println("Name: " + strUser);
+	      } 
 	}
+	
+	/**
+	 * Schliesst Verbindung und Statement.
+	 * 
+	 * @throws Exception
+	 */
+	public void disconnect() throws Exception
+	{
+		_statement.close();
+		_connection.close();
+	}
+	
+	
 	
 }
