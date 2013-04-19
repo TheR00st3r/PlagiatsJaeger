2004a2036c0ff09aaafaac3abce46eac095d61b5
4ipalino <DH10HAH@DH10HAH.homag.com>
2013-04-19 16:21:50 +0200

    loadFile Methode implementiert

--- src/info/plagiatsjaeger/SourceLoader.java
+++ src/info/plagiatsjaeger/SourceLoader.java
@@ -1,5 +1,12 @@
 package info.plagiatsjaeger;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SourceLoader
 {
 
@@ -7,6 +14,43 @@ public class SourceLoader
 	{
 		String result = null;
 
+		return result;
+	}
+	
+	public String loadFile(String filePath)
+	{
+		String result = "";
+		FileInputStream fileInputstream = null;
+		DataInputStream dataInputStream = null;
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		try
+		{
+			String line = "";
+			fileInputstream = new FileInputStream(filePath);
+			dataInputStream = new DataInputStream(fileInputstream);
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dataInputStream));
+			
+			while((line = bufferedReader.readLine()).length() != 0)
+			{
+				stringBuilder.append(line);
+			}
+			// Close the input stream
+			dataInputStream.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			result = stringBuilder.toString();
+		}
 		return result;
 	}
 }
