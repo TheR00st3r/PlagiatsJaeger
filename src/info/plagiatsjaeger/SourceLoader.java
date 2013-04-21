0caea23f6dafe25c31562359e2385dfcb09ec96d
4ipalino <andyhahn91@gmail.com>
2013-04-21 14:29:58 +0200

    - loadUrl as Static - loadUrl implementiert

--- src/info/plagiatsjaeger/SourceLoader.java
+++ src/info/plagiatsjaeger/SourceLoader.java
@@ -6,33 +6,65 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class SourceLoader
 {
 
-	public String loadURL(String url)
+	public static String loadURL(String strUrl)
 	{
-		String result = null;
+		StringBuilder result = new StringBuilder();
 
-		return result;
+		try
+		{
+			URL url = new URL(strUrl);
+			InputStreamReader reader = new InputStreamReader(url.openStream(), "UTF-8");
+
+			BufferedReader bufferedReader = new BufferedReader(reader);
+
+			String line = bufferedReader.readLine();
+			while (line != null)
+			{
+				result.append(line);
+				line = bufferedReader.readLine();
+			}
+
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result.toString();
 	}
-	
+
 	public static String loadFile(String filePath)
 	{
 		String result = "";
 		FileInputStream fileInputstream = null;
 		DataInputStream dataInputStream = null;
-		
+
 		StringBuilder stringBuilder = new StringBuilder();
-		
+
 		try
 		{
 			String line = "";
 			fileInputstream = new FileInputStream(filePath);
 			dataInputStream = new DataInputStream(fileInputstream);
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dataInputStream));
-			
-			while((line = bufferedReader.readLine()) != null)
+
+			while ((line = bufferedReader.readLine()) != null)
 			{
 				stringBuilder.append(line);
 			}
