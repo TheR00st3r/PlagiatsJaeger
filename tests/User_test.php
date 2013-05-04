dc1f7d7134fb5e4b9c378c39bbdfc70c35b44e7f
mark-1003 <markstrecker1003@gmail.com>
2013-05-04 14:49:08 +0200

    added test classes

---
+++ tests/User_test.php
@@ -0,0 +1,13 @@
+<?php
+require_once '/usr/share/php5/PEAR/PHPUnit/Autoload.php';
+require_once 'classes/User.php';
+
+class UserTest extends PHPUnit_Framework_TestCase {
+
+  /*public function test_falseIfNoAtSign() {
+    $actual = Verify::checkEmail('manuel.kiessling.net');
+    $this -> assertFalse($actual);
+  }*/
+
+}
+?>
