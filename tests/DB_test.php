74aaf80ea5cf0824944e2c68ecf0fc02fb23a9f4
mark-1003 <markstrecker1003@gmail.com>
2013-05-11 16:54:42 +0200

    added test_connectToLocalhost renamed Setting_test.php

--- tests/DB_test.php
+++ tests/DB_test.php
@@ -3,11 +3,54 @@ require_once '/usr/share/php5/PEAR/PHPUnit/Autoload.php';
 require_once 'classes/DB.php';
 
 class DBTest extends PHPUnit_Framework_TestCase {
+  protected $logData;
 
-  /*public function test_falseIfNoAtSign() {
-    $actual = Verify::checkEmail('manuel.kiessling.net');
-    $this -> assertFalse($actual);
-  }*/
+  /*
+   * called before every test-function call
+   * set up test environment
+   */
+  protected function setUp() {
+    // setup configuration
+    $this->logData = array('host'     => 'localhost', 
+                           'user'     => 'root',
+                           'pass'     => 'root',
+                           'database' => 'testDB',
+                           'debug'    => true
+                          );
+  
+    // create database
+    $sql = 'CREATE DATABASE '.$this->logData['database'];
+    mysql_connect($this->logData['host'], $this->logData['user'], $this->logData['pass'])
+      or die(mysql_error()); 
+    mysql_query($sql) 
+      or die(mysql_error());
+    mysql_close();
+    
+  }
+  
+  /*
+   * called after every test-function call
+   * clean up test environment
+   */
+  protected function tearDown() {
+    $sql_befehl = 'DROP DATABASE '.$this->logData['database'];
+    mysql_connect($this->logData['host'], $this->logData['user'], $this->logData['pass'])
+      or die(mysql_error()); 
+    mysql_query($sql) 
+      or die(mysql_error());
+    mysql_close();
+    
+    unset($this->logData);
+  }
+  
+  /*
+   * connect to DB
+   * assert if failed
+   */
+  public function test_connectToLocalhost() {
+    $db = new DB($this->logData);
+    $this -> assertFalse($db);
+  }
 
 }
 
---
+++ tests/Setting_test.php
@@ -0,0 +1,15 @@
+<?php
+require_once '/usr/share/php5/PEAR/PHPUnit/Autoload.php';
+require_once 'classes/Setting.php';
+
+class SettingTest extends PHPUnit_Framework_TestCase {
+
+  /*public function test_falseIfNoAtSign() {
+    $actual = Verify::checkEmail('manuel.kiessling.net');
+    $this -> assertFalse($actual);
+  }*/
+
+}
+
+
+?>
