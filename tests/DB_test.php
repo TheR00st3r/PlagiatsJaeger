872e0508ebaa23bc77c69f26ef43158294c59102
mark-1003 <markstrecker1003@gmail.com>
2013-05-17 13:27:58 +0200

    -updated DB_test.php

--- tests/DB_test.php
+++ tests/DB_test.php
@@ -1,9 +1,11 @@
 <?php
 require_once '/usr/share/php5/PEAR/PHPUnit/Autoload.php';
-require_once 'classes/DB.php';
+require_once '../configs/setup.php';
 
 class DBTest extends PHPUnit_Framework_TestCase {
-  protected $logData;
+  protected $database ="testDB";
+  
+  
 
   /*
    * called before every test-function call
@@ -11,16 +13,17 @@ class DBTest extends PHPUnit_Framework_TestCase {
    */
   protected function setUp() {
     // setup configuration
-    $this->logData = array('host'     => 'localhost', 
+    /*$this->logData = array('host'     => 'localhost', 
                            'user'     => 'root',
                            'pass'     => 'root',
                            'database' => 'testDB',
                            'debug'    => true
-                          );
+                          );*/
+    global $logData;
   
     // create database
-    $sql = 'CREATE DATABASE '.$this->logData['database'];
-    mysql_connect($this->logData['host'], $this->logData['user'], $this->logData['pass'])
+    $sql = 'CREATE DATABASE '.$database;
+    mysql_connect($logData['host'], $logData['user'], $logData['pass'])
       or die(mysql_error()); 
     mysql_query($sql) 
       or die(mysql_error());
@@ -33,14 +36,12 @@ class DBTest extends PHPUnit_Framework_TestCase {
    * clean up test environment
    */
   protected function tearDown() {
-    $sql_befehl = 'DROP DATABASE '.$this->logData['database'];
-    mysql_connect($this->logData['host'], $this->logData['user'], $this->logData['pass'])
+    $sql_befehl = 'DROP DATABASE '.$database;
+    mysql_connect($logData['host'], $logData['user'], $logData['pass'])
       or die(mysql_error()); 
     mysql_query($sql) 
       or die(mysql_error());
     mysql_close();
-    
-    unset($this->logData);
   }
   
   /*
@@ -48,7 +49,7 @@ class DBTest extends PHPUnit_Framework_TestCase {
    * assert if failed
    */
   public function test_connectToLocalhost() {
-    $db = new DB($this->logData);
+    $db = new DB($logData);
     $this -> assertFalse($db);
   }
 
