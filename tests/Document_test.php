78083ae7332151fde597efcad2b26c7f65052d8c
mark-1003 <markstrecker1003@gmail.com>
2013-05-17 14:04:06 +0200

    - added setUp and tearDown

--- tests/Document_test.php
+++ tests/Document_test.php
@@ -3,12 +3,26 @@ require_once '/usr/share/php/PHPUnit/Autoload.php';
 require_once '../configs/setup.php';
 
 class DocumentTest extends PHPUnit_Framework_TestCase {
+  
+  protected function setUp() {
+    echo "setUp()";
+  }
+  
+  /*
+   * called after every test-function call
+   * clean up test environment
+   */
+  protected function tearDown() {
+    echo "tearDown()";
+  }
 
   public function test_Test1() {
+    echo "test_Test1()";
     $this -> assertFalse(true);
   }
   
   public function test_Test2() {
+    echo "test_Test2()";
     $this -> assertFalse(false);
   }
 
