3e38e6705face11899f7f9d391866577e201df43
4ipalino <DH10HAH@DH10HAH.homag.com>
2013-05-17 14:11:02 +0200

    libs to webinf added

---
+++ src/test/info/plagiatsjaeger/FileParserLokalTest.java
@@ -0,0 +1,14 @@
+package test.info.plagiatsjaeger;
+
+import info.plagiatsjaeger.FileParser;
+
+public class FileParserLokalTest
+{
+
+	public static void main(String[] args)
+	{
+		FileParser fp = new FileParser();
+		fp.parseFile("");
+	}
+
+}
