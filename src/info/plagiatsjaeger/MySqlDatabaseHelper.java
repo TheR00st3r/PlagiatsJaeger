56be562314b725b23f37e113a21c933101f701f1
K47S <michi.volz@gmx.net>
2013-05-21 12:43:00 +0200

    encoding corrected

--- src/info/plagiatsjaeger/MySqlDatabaseHelper.java
+++ src/info/plagiatsjaeger/MySqlDatabaseHelper.java
@@ -122,7 +122,7 @@ public class MySqlDatabaseHelper
 			DecimalFormat df = new DecimalFormat("###.##", otherSymbols);
 			for (CompareResult result : compareResults)
 			{
-				String text = new String(Charset.forName("Cp1252").encode(result.getSourceText()).array());
+				String text = new String(Charset.forName("Cp1252").encode(result.getSourceText()).array(), "UTF-8 ");
 				//String text = new String(result.getSourceText().getBytes("UTF-8"), "ISO-8859-1");
 				strStatement = "INSERT INTO result VALUES(DEFAULT, '" + text + "' , '" + "' , '" + dID + "' , '" + result.getCheckStart() + "' , '" + result.getCheckEnd() + "' , '" + df.format(result.getSimilarity() * 100) + "' , '" + (result.getIsInSources() ? 1 :0 ) + "' , '" + result.getReportID() + "' )";
 				_statement.executeUpdate(strStatement);
@@ -134,6 +134,10 @@ public class MySqlDatabaseHelper
 			_logger.fatal(e.getMessage(), e);
 		}
 		catch (SQLException e)
+		{
+			_logger.fatal(e.getMessage(), e);
+		}
+		catch (UnsupportedEncodingException e)
 		{
 			_logger.fatal(e.getMessage(), e);
 		}
@@ -160,7 +164,7 @@ public class MySqlDatabaseHelper
 			DecimalFormat df = new DecimalFormat("###.##", otherSymbols);
 			for (CompareResult result : compareResults)
 			{
-				String text = new String(Charset.forName("Cp1252").encode(result.getSourceText()).array());
+				String text = new String(Charset.forName("Cp1252").encode(result.getSourceText()).array(), "UTF-8 ");
 				//String text = new String(result.getSourceText().getBytes("UTF-8"), "ISO-8859-1");
 
 				_logger.info("Text: " + result.getSourceText());
@@ -174,6 +178,10 @@ public class MySqlDatabaseHelper
 			_logger.fatal(e.getMessage(), e);
 		}
 		catch (SQLException e)
+		{
+			_logger.fatal(e.getMessage(), e);
+		}
+		catch (UnsupportedEncodingException e)
 		{
 			_logger.fatal(e.getMessage(), e);
 		}
