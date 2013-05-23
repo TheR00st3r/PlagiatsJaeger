b10a1ac6eb4ce704b818f91e511c37d19d62ab14
mark-1003 <markstrecker1003@gmail.com>
2013-05-23 19:17:31 +0200

    - added test function to User_test.php

--- tests/User_test.php
+++ tests/User_test.php
@@ -2,7 +2,11 @@
 require_once '/usr/share/php/PHPUnit/Autoload.php';
 require_once '../configs/setup.php';
 
+require_once '../classes/User.php';
+
+
 class UserTest extends PHPUnit_Framework_TestCase {
+  
   
   /*
    * called before every test-function call
@@ -12,6 +16,7 @@ class UserTest extends PHPUnit_Framework_TestCase {
     echo "setUp()";
   }
   
+  
   /*
    * called after every test-function call
    * clean up test environment
@@ -19,13 +24,79 @@ class UserTest extends PHPUnit_Framework_TestCase {
   protected function tearDown() {
     echo "tearDown()";
   }
+  
+  
+  /*
+   * create an object of User class
+   */
+  public function test_createObject() {
+    $userObject = null;
+    $userObject = new User();
+    
+    if ($userObject != null) {
+      $assert = true;
+    }
+    else {
+      $assert = false;
+    }
+    
+    $this->assertEquals(true, $assert);
+  }
 
-  public function test_Test1() {
-    $this -> assertFalse(true);
+
+  /*
+   * check if getAllUsers returns at least one value
+   */
+  public function test_getAllUsersTest() {
+    $userObject = null;
+    $userObject = new User();
+    
+    if ($userObject != null) {
+      $return = $userObject->getAllUsers();
+      if ( count($return) > 0 ) {
+        $assert = true;
+      }
+      else {
+        $assert = false;
+      }
+    }
+    else {
+      $assert = false;
+    }
+    
+    $this->assertEquals(true, $assert);
   }
   
-  public function test_Test2() {
-    $this -> assertFalse(false);
+  
+  /*
+   * get all users and take the id from the first [0] to get it again by getUser()
+   * check if the name is the same
+   */
+  public function test_getUser() {
+    $userObject = null;
+    $userObject = new User();
+    
+    if ($userObject != null) {
+      $return = $userObject->getAllUsers();
+      if ( count($return) > 0 ) {
+        $returnUser = $userObject->getUser($return[0]['uID']);  
+        
+        if ( $return[0]['uName'] == $returnUser['uName'] ) {
+          $assert = true;  
+        }
+        else {
+          $assert = false;
+        }
+      }
+      else {
+        $assert = false;
+      }
+    }
+    else {
+      $assert = false;
+    }
+    
+    $this->assertEquals(true, $assert);
   }
 
 }
--- tests/Validator_test.php
+++ tests/Validator_test.php
@@ -25,7 +25,7 @@ class ValidatorTest extends PHPUnit_Framework_TestCase {
   /*
    * check if 'abCdeFghiJKLMNOPQ()...' identified as String
    */
-  public function test_IsString() {
+  public function test_isString() {
     $assert = Validator::validate(VAL_STRING, 'abCdeFghiJKLMNOPQ()...', true);
     $this->assertEquals(true, $assert);
   }
@@ -34,7 +34,7 @@ class ValidatorTest extends PHPUnit_Framework_TestCase {
   /*
    * check if 'max@mustermann.de' is identified as an Email
    */
-  public function test_IsEmail() {
+  public function test_isEmail() {
     $assert = Validator::validate(VAL_EMAIL, 'max@mustermann.de', true);
     $this->assertEquals(true, $assert);
   }
@@ -43,7 +43,7 @@ class ValidatorTest extends PHPUnit_Framework_TestCase {
   /*
    * check if '10' is identified as an Integer
    */
-  public function test_IsInteger() {
+  public function test_isInteger() {
     $assert = Validator::validate(VAL_INTEGER, '10', true);
     $this->assertEquals(true, $assert);
   }
@@ -52,7 +52,7 @@ class ValidatorTest extends PHPUnit_Framework_TestCase {
   /*
    * check if 'Hallo, ich bin kein Integer! :-P' is identified as NO Integer
    */
-  public function test_IsNotInteger() {
+  public function test_isNotInteger() {
     $assert = Validator::validate(VAL_INTEGER, 'Hallo, ich bin kein Integer! :-P', true);
     $this->assertEquals(false, $assert);
   }
@@ -61,7 +61,7 @@ class ValidatorTest extends PHPUnit_Framework_TestCase {
   /*
    * check if emptyString is identified as null
    */
-  public function test_IsNotEmpty() {
+  public function test_isNotEmpty() {
     $assert = Validator::validate(VAL_STRING, '', true);
     $this->assertEquals(false, $assert);
   }
