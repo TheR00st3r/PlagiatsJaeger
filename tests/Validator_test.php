4722b5fe294243f1067349c4e84e6f188ef907b0
mark-1003 <markstrecker1003@gmail.com>
2013-05-23 18:06:00 +0200

    - comments added

--- tests/Validator_test.php
+++ tests/Validator_test.php
@@ -22,30 +22,45 @@ class ValidatorTest extends PHPUnit_Framework_TestCase {
   }
 
 
+  /*
+   * check if 'abCdeFghiJKLMNOPQ()...' identified as String
+   */
   public function test_IsString() {
     $assert = Validator::validate(VAL_STRING, 'abCdeFghiJKLMNOPQ()...', true);
     $this->assertEquals(true, $assert);
   }
   
   
+  /*
+   * check if 'max@mustermann.de' is identified as an Email
+   */
   public function test_IsEmail() {
     $assert = Validator::validate(VAL_EMAIL, 'max@mustermann.de', true);
     $this->assertEquals(true, $assert);
   }
   
   
+  /*
+   * check if '10' is identified as an Integer
+   */
   public function test_IsInteger() {
     $assert = Validator::validate(VAL_INTEGER, '10', true);
     $this->assertEquals(true, $assert);
   }
   
   
+  /*
+   * check if 'Hallo, ich bin kein Integer! :-P' is identified as NO Integer
+   */
   public function test_IsNotInteger() {
     $assert = Validator::validate(VAL_INTEGER, 'Hallo, ich bin kein Integer! :-P', true);
     $this->assertEquals(false, $assert);
   }
   
   
+  /*
+   * check if emptyString is identified as null
+   */
   public function test_IsNotEmpty() {
     $assert = Validator::validate(VAL_STRING, '', true);
     $this->assertEquals(false, $assert);
