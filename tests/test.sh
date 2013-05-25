d7d4eb9a8e087f582475f406338d617f423e4c46
mark-1003 <markstrecker1003@gmail.com>
2013-05-25 16:37:03 +0200

    -added --log-junit <file>

--- tests/test.sh
+++ tests/test.sh
@@ -3,5 +3,6 @@
 
 cd /var/www/tests
 
-phpunit --coverage-html /usr/share/tomcat7/.jenkins/jobs/PlagiatsJaegerPHP/workspace/reports --coverage-clover /usr/share/tomcat7/.jenkins/jobs/PlagiatsJaegerPHP/workspace/reports/clover.xml ./
+#phpunit --coverage-html /usr/share/tomcat7/.jenkins/jobs/PlagiatsJaegerPHP/workspace/reports --coverage-clover /usr/share/tomcat7/.jenkins/jobs/PlagiatsJaegerPHP/workspace/reports/clover.xml --log-junit /usr/share/tomcat7/.jenkins/jobs/PlagiatsJaegerPHP/workspace/reports/junit/junit.xml ./
+phpunit --coverage-clover /usr/share/tomcat7/.jenkins/jobs/PlagiatsJaegerPHP/workspace/reports/clover.xml ./
 
