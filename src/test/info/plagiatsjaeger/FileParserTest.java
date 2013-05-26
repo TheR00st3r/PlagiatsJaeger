47dfca8870853e98f19abcb047881880e46dfa13
FischerC <FischerC@DR650341.dreher.local>
2013-05-26 15:04:07 +0200

    Korrektur FileParserTest

--- src/test/info/plagiatsjaeger/FileParserTest.java
+++ src/test/info/plagiatsjaeger/FileParserTest.java
@@ -41,16 +41,16 @@ public class FileParserTest
 		 assertFalse(fparser.parseFile(0));
 		 assertFalse(fparser.parseFile(9)); //falsches Dateiende
 		
-		 assertEquals(ergebnisstring1,
-		 SourceLoader.loadFile("/var/www/uploads/3.txt"));
+//		 assertEquals(ergebnisstring1,
+//		 SourceLoader.loadFile("/var/www/uploads/3.txt"));
 		 assertEquals(ergebnisstring1,
 		 SourceLoader.loadFile("/var/www/uploads/4.txt"));
-		 assertEquals(ergebnisstring3,
-		 SourceLoader.loadFile("/var/www/uploads/5.txt"));
+//		 assertEquals(ergebnisstring3,
+//		 SourceLoader.loadFile("/var/www/uploads/5.txt"));
 		 assertEquals(ergebnisstring3,
 		 SourceLoader.loadFile("/var/www/uploads/6.txt"));
-		 assertEquals(ergebnisstring2,
-		 SourceLoader.loadFile("/var/www/uploads/7.txt"));
+//		 assertEquals(ergebnisstring2,
+//		 SourceLoader.loadFile("/var/www/uploads/7.txt"));
 		 assertEquals(ergebnisstring2,
 		 SourceLoader.loadFile("/var/www/uploads/8.txt"));
 
