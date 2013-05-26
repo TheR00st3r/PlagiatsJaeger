bd27d273133dfef8880cecdc84d33c893a19ea4d
FischerC <FischerC@DR650341.dreher.local>
2013-05-26 16:36:00 +0200

    Weiter JUnit

--- src/test/info/plagiatsjaeger/SourceLoaderTest.java
+++ src/test/info/plagiatsjaeger/SourceLoaderTest.java
@@ -26,20 +26,21 @@ public class SourceLoaderTest
 
 		String teststring1 = "http://192.168.4.28/testfiles/testseite.html";
 		String teststring2 = "http://www.assdsdadasdasdasda.de";
-//		String teststring3 = "www.plagiatsjaeger.de.de.de";
+		String teststring3 = "httpxyz://www.plagiatsjaeger.de";
 		String teststring4 = "";
-//		String teststring5 = "www.plagiatsjaeger.info/projektplan/projektteam/";
-//		String teststring6 = "plagiatsjaeger.info/projektplan/projektteam/";
+		// String teststring5 =
+		// "www.plagiatsjaeger.info/projektplan/projektteam/";
+		// String teststring6 = "plagiatsjaeger.info/projektplan/projektteam/";
 		// String teststring7 = "http://de.wikipedia.org/wiki/Das_B�se";
 
 		assertEquals(ergebnisstring1, SourceLoader.loadURL(teststring1, true, false));
-//		 assertTrue(SourceLoader.loadURL(teststring7).contains(ergebnisstring2));
-		 assertTrue(SourceLoader.loadURL(teststring2).contains("FAIL IOException"));
-//		 System.out.println(SourceLoader.loadURL(teststring3));
-//		 assertTrue(SourceLoader.loadURL(teststring3).contains("FAIL MalformedURLException"));
-		 assertTrue(SourceLoader.loadURL(teststring4).contains("FAIL MalformedURLException"));
-//		 assertTrue(SourceLoader.loadURL(teststring5).contains("FAIL MalformedURLException"));
-//		 assertTrue(SourceLoader.loadURL(teststring6).contains("FAIL MalformedURLException"));
+		// assertTrue(SourceLoader.loadURL(teststring7).contains(ergebnisstring2));
+		assertTrue(SourceLoader.loadURL(teststring2).contains("FAIL IOException"));
+		// System.out.println(SourceLoader.loadURL(teststring3));
+		assertTrue(SourceLoader.loadURL(teststring3).contains("FAIL MalformedURLException"));
+		assertTrue(SourceLoader.loadURL(teststring4).contains("FAIL MalformedURLException"));
+		// assertTrue(SourceLoader.loadURL(teststring5).contains("FAIL MalformedURLException"));
+		// assertTrue(SourceLoader.loadURL(teststring6).contains("FAIL MalformedURLException"));
 
 	}
 
@@ -47,20 +48,20 @@ public class SourceLoaderTest
 	public void testLoadFile()
 	{
 
-//		String ergebnisstring1 = "";
-//		String ergebnisstring2 = "Hallo, ich bin ein/nZeilenumbruch";
-//		String ergebnisstring3 = "Gesperrtes File";
-//		// String ergebnisstring4 =
-//		// "D�ner mit So�e & einer b�rigen t�rkischen Bananen & Co KG";
-//
-//		 assertTrue(SourceLoader.loadFile("/var/www/testfiles/testfile3.txt").contains("FAIL FileNotFoundException"));
-//		 assertEquals(ergebnisstring1,
-//		 SourceLoader.loadFile("/var/www/testfiles/testfile1.txt"));
-//		 assertEquals(ergebnisstring2,
-//		 SourceLoader.loadFile("/var/www/testfiles/fehlendesfile.txt"));
-//		 assertTrue(SourceLoader.loadFile("/var/www/testfiles/testfile3.txt").contains("FAIL IOException"));
-////		 assertEquals(ergebnisstring4,
-////		 SourceLoader.loadFile("/var/www/testfiles/testfile4.txt"));
+		// String ergebnisstring1 = "";
+		// String ergebnisstring2 = "Hallo, ich bin ein/nZeilenumbruch";
+		// String ergebnisstring3 = "Gesperrtes File";
+		// // String ergebnisstring4 =
+		// // "D�ner mit So�e & einer b�rigen t�rkischen Bananen & Co KG";
+		//
+		// assertTrue(SourceLoader.loadFile("/var/www/testfiles/testfile3.txt").contains("FAIL FileNotFoundException"));
+		// assertEquals(ergebnisstring1,
+		// SourceLoader.loadFile("/var/www/testfiles/testfile1.txt"));
+		// assertEquals(ergebnisstring2,
+		// SourceLoader.loadFile("/var/www/testfiles/fehlendesfile.txt"));
+		// assertTrue(SourceLoader.loadFile("/var/www/testfiles/testfile3.txt").contains("FAIL IOException"));
+		// // assertEquals(ergebnisstring4,
+		// // SourceLoader.loadFile("/var/www/testfiles/testfile4.txt"));
 
 	}
 
