6ab0798b75e7c97252bd760c1bafec6667738d77
FischerC <FischerC@DR650341.dreher.local>
2013-05-26 16:57:21 +0200

    JUnitTests

--- src/test/info/plagiatsjaeger/SourceLoaderTest.java
+++ src/test/info/plagiatsjaeger/SourceLoaderTest.java
@@ -56,7 +56,7 @@ public class SourceLoaderTest
 		// // "D�ner mit So�e & einer b�rigen t�rkischen Bananen & Co KG";
 		//
 		 System.out.println(SourceLoader.loadFile("/var/www/testfiles/testfile3.txt"));
-		 assertTrue(SourceLoader.loadFile("/var/www/testfiles/fehlendesfile.txt").contains("FAIL FileNotFoundException"));
+		 assertTrue(SourceLoader.loadFile("/var/www/testfiles/fehlendesfile.txt").contains("FAIL IOException"));
 		 System.out.println(SourceLoader.loadFile("/var/www/testfiles/testfile1.txt"));
 		 assertEquals(ergebnisstring1,
 		 SourceLoader.loadFile("/var/www/testfiles/testfile1.txt"));
