0790fd197ae11165a43a8427ea7ed974c5160e13
schnti <timo@schnti.de>
2013-05-28 12:42:05 +0200

    Bugfix.

--- classes/Result.php
+++ classes/Result.php
@@ -19,7 +19,7 @@ class Result {
 				FROM
 					result AS rt LEFT JOIN
 					document AS d ON rt.rtSourcedID = d.dID LEFT JOIN
-					folderpermission AS fp ON fp.fID = d.fID LEFT JOIN
+					folderpermission AS fp ON fp.fID = d.fID AND fp.fpPermissionLevel = 900 LEFT JOIN
 					folder AS f ON fp.fID = f.fID LEFT JOIN
 					user AS u ON fp.uID = u.uID
 				WHERE
